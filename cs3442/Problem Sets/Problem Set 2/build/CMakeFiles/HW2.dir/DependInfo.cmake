60999ecdb12d6d4b7e853c46ae3496f015d268b8
yanring <yanzijie1996@gmail.com>
2019-03-19 16:26:51 +0800

    fix

---
+++ cs3442/Problem Sets/Problem Set 2/build/CMakeFiles/HW2.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/share/CUDA-Course-Udacity/cs344/Problem Sets/Problem Set 2/compare.cpp" "/share/CUDA-Course-Udacity/cs344/Problem Sets/Problem Set 2/build/CMakeFiles/HW2.dir/compare.o"
+  "/share/CUDA-Course-Udacity/cs344/Problem Sets/Problem Set 2/main.cpp" "/share/CUDA-Course-Udacity/cs344/Problem Sets/Problem Set 2/build/CMakeFiles/HW2.dir/main.o"
+  "/share/CUDA-Course-Udacity/cs344/Problem Sets/Problem Set 2/reference_calc.cpp" "/share/CUDA-Course-Udacity/cs344/Problem Sets/Problem Set 2/build/CMakeFiles/HW2.dir/reference_calc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "/opt/cuda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
